0e5859ed17e9e0a8bc3343f6a1412edcfaf82f3f
jerry <jerrynwosu007@gmail.com>
2022-06-27 15:21:30 +0100

    adding components to settings

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -4,5 +4,20 @@ import GearIcon from './GearIcon';
 import LogoutIcon from './LogoutIcon';
 import ArchiveIcon from './ArchiveIcon';
 import ControlIcon from './ControlIcon';
+import CancelIcon from './CancelIcon';
+import BellIcon from './BellIcon';
+import PaintbrushIcon from './PaintbrushIcon';
+import AccountIcon from './AccountIcon';
 
-export { TrashIcon, LabelIcon, GearIcon, LogoutIcon, ArchiveIcon, ControlIcon };
+export {
+  TrashIcon,
+  LabelIcon,
+  GearIcon,
+  LogoutIcon,
+  ArchiveIcon,
+  ControlIcon,
+  CancelIcon,
+  AccountIcon,
+  PaintbrushIcon,
+  BellIcon,
+};
--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { setThemeMode } from '../../features/actionSlice';
 import styled from 'styled-components';
+import { AccountIcon, BellIcon, CancelIcon, PaintbrushIcon } from '../icons';
 
 const Settings = () => {
   const dispatch = useDispatch();
+
+  const tabList = [
+    { name: 'Account', alias: 'account', element: <AccountIcon /> },
+    { name: 'Appearance', alias: 'appearance', element: <PaintbrushIcon /> },
+    { name: 'Notification', alias: 'notification', element: <BellIcon /> },
+  ];
 
   const handleTheme = (mode) => {
     dispatch(setThemeMode(mode));
@@ -13,13 +20,31 @@ const Settings = () => {
   return (
     <SettingsWrap>
       <Overlay />
-      <SettingsMain>
-        <div>This is settings</div>
-        <div>
-          <button onClick={() => handleTheme('dark')}>Dark</button>
-          <button onClick={() => handleTheme('light')}>Light</button>
-        </div>
-      </SettingsMain>
+      <SettingsInner>
+        <SettingsSide>
+          <div className="tabList">
+            {tabList.map((tab) => (
+              <div className="tabItem" key={tab.alias}>
+                <div className="tabIcon">{tab.element}</div>
+                <div className="tabName">{tab.name}</div>
+              </div>
+            ))}
+          </div>
+        </SettingsSide>
+        <SettingsMain>
+          <SettingsNav>
+            <div className="title">Tab Title</div>
+            <div className="close">
+              <CancelIcon />
+            </div>
+          </SettingsNav>
+          <div>This is settings</div>
+          <div>
+            <button onClick={() => handleTheme('dark')}>Dark</button>
+            <button onClick={() => handleTheme('light')}>Light</button>
+          </div>
+        </SettingsMain>
+      </SettingsInner>
     </SettingsWrap>
   );
 };
@@ -35,13 +60,78 @@ const SettingsWrap = styled.div`
   left: 0;
   top: 0;
 `;
-const SettingsMain = styled.div`
+const SettingsInner = styled.div`
   width: 900px;
   height: 600px;
-  background-color: ${(props) => props.theme.colors.tertiary};
+  max-width: 100%;
+  max-height: 100%;
+  background-color: ${(props) => props.theme.colors.secondary};
   border: 1px solid ${(props) => props.theme.colors.border_color1};
   border-radius: ${(props) => props.theme.reset.border_radius};
+  display: flex;
 `;
+const SettingsSide = styled.div`
+  width: 200px;
+  height: 100%;
+  background-color: ${(props) => props.theme.colors.tertiary};
+  padding: 10px 0;
+
+  .tabList {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .tabItem {
+    display: flex;
+    padding: 8px 15px;
+    align-items: center;
+    cursor: pointer;
+
+    :hover {
+      background-color: ${(props) => props.theme.colors.hover_color1};
+    }
+  }
+
+  .tabIcon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    svg {
+      width: 20px;
+      fill: ${(props) => props.theme.colors.text_color2};
+    }
+  }
+
+  .tabName {
+    margin-left: 8px;
+    font-size: 14px;
+    color: ${(props) => props.theme.colors.text_color2};
+  }
+`;
+const SettingsMain = styled.div`
+  flex: 1;
+`;
+
+const SettingsNav = styled.div`
+  height: 50px;
+  width: 100%;
+  border-bottom: 1px solid ${(props) => props.theme.colors.border_color1};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+
+  .close {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    svg {
+      fill: ${(props) => props.theme.colors.text_color_default};
+      width: 20px;
+    }
+  }
+`;
+
 const Overlay = styled.div``;
 
 export default Settings;
