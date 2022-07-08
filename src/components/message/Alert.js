ea5821298e2c30acbfc8a801d11aaaec3666ce77
jerry <jerrynwosu007@gmail.com>
2022-07-08 23:26:03 +0100

    some more juice

---
+++ src/components/message/Alert.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Alert = ({ msg }) => {
+  return (
+    <div>
+      <div>{msg}</div>
+    </div>
+  );
+};
+
+export default Alert;
