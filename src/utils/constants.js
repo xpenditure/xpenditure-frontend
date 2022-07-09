52e94a6ca9070ba86eae04ca4f5bc68c172b862f
jerry <jerrynwosu007@gmail.com>
2022-07-09 04:26:41 +0100

    fixed socket local env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ import { getFromLS } from './storage';
 
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
-    ? `http://localhost:8000/api`
-    : 'https://xpenditure-api.herokuapp.com/api';
+    ? `http://localhost:8000`
+    : 'https://xpenditure-api.herokuapp.com';
 
 export const TOKEN = getFromLS('token');
