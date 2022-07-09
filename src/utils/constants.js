0469c1f917f12b5a5beeb02b8fc10676cee4dbf3
jerry <jerrynwosu007@gmail.com>
2022-07-09 04:05:27 +0100

    some evn settings

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 import { getFromLS } from './storage';
 
-export const BASE_URL = `http://localhost:8000/api`;
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:8000/api`
+    : 'https://xpenditure-api.herokuapp.com/api';
 
 export const TOKEN = getFromLS('token');
